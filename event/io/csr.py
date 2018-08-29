3a8187f8ef6c3833e5bcdad20c706c5e5d0e4131
shawnkx <xkong12@illinois.edu>
2018-08-29 14:25:38 -0400

    csr

--- event/io/csr.py
+++ event/io/csr.py
@@ -822,7 +822,7 @@ class CSR:
                 return 'aida', 'Time'
         return onto_name, entity_type
 
-    def add_relation(self, head_span,  ontology, arguments, 
+    def add_relation(self, span,  ontology, arguments, 
                      relation_type, component=None, relation_id=None):
         """
         Adding a relation to csr.
